147ac256d0d810da5e1f117c34b56bdf860b7083
Daniel Stormont <daniel.stormont@gmail.com>
2020-12-11 23:19:19 -0700

    Add test data.

--- scripts/game.js
+++ scripts/game.js
@@ -38,24 +38,24 @@ let plasticsCollected = {
 // plastic, and six randomly selected countries - some of which will satisfy
 // the mission requirements and some that won't.
 let mission = {
-    plasticType: "",
+    plasticType: '',
     numPieces: 0,
-    product: "",
+    product: '',
     countries: {
-        country1: "",
-        country2: "",
-        country3: "",
-        country4: "",
-        country5: "",
-        country6: ""
+        country1: '',
+        country2: '',
+        country3: '',
+        country4: '',
+        country5: '',
+        country6: ''
     }
 };
 
 // hideSplash
 // Hides the splashscreen and displays the mission screen for the first mission.
 function hideSplash() {
-    let splash = document.getElementById("splashscreen");
-    splash.style.display = "none";
+    let splash = document.getElementById('splashscreen');
+    splash.style.display = 'none';
     generateMission();
     displayMissionScreen();
 }
@@ -68,37 +68,39 @@ function hideSplash() {
 // high likelihood of being able to achieve the mission and the others will have
 // a low likelihood, as determined by parsing the plastics data.
 function generateMission () {
-    console.log("Generating mission.");
+    console.log('Generating mission.');
     // TODO: Logic for populating the mission object.
-    mission.plasticType = "Plastic Beverage Bottles";
+    mission.plasticType = 'Plastic Beverage Bottles';
     mission.numPieces = 117962;
-    mission.product = "Toothbrushes",
+    mission.product = 'Toothbrushes',
     mission.countries = {
-        country1: "Canada",
-        country2: "Jamaica",
-        country3: "Venezuela",
-        country4: "United States",
-        country5: "India",
-        country6: "China"
+        country1: 'Canada',
+        country2: 'Jamaica',
+        country3: 'Venezuela',
+        country4: 'United States',
+        country5: 'India',
+        country6: 'China'
     };
 }
 
-let missionScreen = document.getElementById("missionscreen");
+let missionScreen = document.getElementById('missionscreen');
 
 // displayMissionScreen
 // Display a mission to the player. The player can choose
 // whether or not to accept the mission via the mission button event listeners. 
 function displayMissionScreen() {
-    missionScreen.style.display = "grid";
-    document.getElementById("mission-num-pieces").textContent = mission.numPieces;
-    document.getElementById("mission-plastic-type").textContent = mission.plasticType;
-    document.getElementById("plastic-product").textContent = mission.product;
+    missionScreen.style.display = 'grid';
+    document.getElementById('mission-num-pieces').textContent = mission.numPieces;
+    document.getElementById('mission-plastic-type').textContent = mission.plasticType;
+    document.getElementById('plastic-product').textContent = mission.product;
+    document.getElementById('plastic-image').src='images/plastics/plastic-beverage-bottle.png';
+    document.getElementById('product-image').src='images/products/toothbrushes.png';
 }
 
 // hideMissionScreen
 // Hide the mission screen.
 function hideMissionScreen() {
-    missionScreen.style.display = "none";
+    missionScreen.style.display = 'none';
 }
 
 // highlightGameboard
@@ -109,28 +111,28 @@ function highlightGameboard() {
     window.setTimeout(displayCountryScreen, 500);
 }
 
-let countryScreen = document.getElementById("countryscreen");
+let countryScreen = document.getElementById('countryscreen');
 
 // displayCountryScreen
 // Display the country selection screen. The player selects a country via the
 // country button event listeners.
 function displayCountryScreen() {
-    countryScreen.style.display = "grid";
-    document.getElementById("country-num-pieces").textContent = mission.numPieces;
-    document.getElementById("country-plastic-type").textContent = mission.plasticType;
-    document.getElementById("country-button-1").textContent = mission.countries.country1;
-    document.getElementById("country-button-2").textContent = mission.countries.country2;
-    document.getElementById("country-button-3").textContent = mission.countries.country3;
-    document.getElementById("country-button-4").textContent = mission.countries.country4;
-    document.getElementById("country-button-5").textContent = mission.countries.country5;
-    document.getElementById("country-button-6").textContent = mission.countries.country6;
+    countryScreen.style.display = 'grid';
+    document.getElementById('country-num-pieces').textContent = mission.numPieces;
+    document.getElementById('country-plastic-type').textContent = mission.plasticType;
+    document.getElementById('country-button-1').textContent = mission.countries.country1;
+    document.getElementById('country-button-2').textContent = mission.countries.country2;
+    document.getElementById('country-button-3').textContent = mission.countries.country3;
+    document.getElementById('country-button-4').textContent = mission.countries.country4;
+    document.getElementById('country-button-5').textContent = mission.countries.country5;
+    document.getElementById('country-button-6').textContent = mission.countries.country6;
 
 }
 
 // hideCountryScreen
 // Hide the country selection screen. 
 function hideCountryScreen() {
-    countryScreen.style.display = "none";
+    countryScreen.style.display = 'none';
 }
 
 // sleep
@@ -142,61 +144,61 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let executionScreen = document.getElementById("missionexecutionscreen");
+let executionScreen = document.getElementById('missionexecutionscreen');
 
 // executeMission
 // Display the mission execution screen and execute the mission for the country
 // selected by the player.
 async function executeMission(missionString) {
     console.log(missionString);
-    executionScreen.style.display = "grid";
-    document.getElementById("execution-num-pieces").textContent = mission.numPieces;
-    document.getElementById("execution-plastic-type").textContent = mission.plasticType;
+    executionScreen.style.display = 'grid';
+    document.getElementById('execution-num-pieces').textContent = mission.numPieces;
+    document.getElementById('execution-plastic-type').textContent = mission.plasticType;
     await sleep(5000);
-    executionScreen.style.display = "none";
+    executionScreen.style.display = 'none';
     displayStatisticsScreen();
 }
 
-let confirmationScreen = document.getElementById("confirmationscreen");
+let confirmationScreen = document.getElementById('confirmationscreen');
 
 // displayConfirmationScreen
 // Display the player decision confirmation screen if the player declines a
 // mission. The player can confirm or not confirm the decision using the
 // confirmation button event listeners.
 function displayConfirmationScreen() {
-    confirmationScreen.style.display = "grid";
+    confirmationScreen.style.display = 'grid';
 }
 
 // hideConfirmationScreen
 // Hide the confirmation screen.
 function hideConfirmationScreen() {
-    confirmationScreen.style.display = "none";
+    confirmationScreen.style.display = 'none';
 }
 
-let statisticsScreen = document.getElementById("statisticsscreen");
+let statisticsScreen = document.getElementById('statisticsscreen');
 
 // displayStatisticsScreen
 // Display the game statistics to the player at the end of the game.
 async function displayStatisticsScreen() {
-    statisticsScreen.style.display = "grid";
+    statisticsScreen.style.display = 'grid';
     await sleep(5000);
-    statisticsScreen.style.display = "none";
+    statisticsScreen.style.display = 'none';
     displayPlayAgainScreen();
 }
 
-let playAgainScreen = document.getElementById("playagainscreen");
+let playAgainScreen = document.getElementById('playagainscreen');
 
 // displayPlayAgainScreen
 // Display a prompt to the player to play the game again or not.
 // The player makes a choice using the 'yes' or 'no' button event listeners.
 function displayPlayAgainScreen() {
-    playAgainScreen.style.display = "grid";
+    playAgainScreen.style.display = 'grid';
 }
 
 // hidePlayAgainScreen
 // Hides the play again screen.
 function hidePlayAgainScreen() {
-    playAgainScreen.style.display = "none";
+    playAgainScreen.style.display = 'none';
 }
 
 // clearGameStats
@@ -208,12 +210,12 @@ function clearGameStats() {
     }
 }
 
-let creditScreen = document.getElementById("creditscreen");
+let creditScreen = document.getElementById('creditscreen');
 
 // displayCreditScreen
 // Displays the credit screen.
 function displayCreditScreen() {
-    creditScreen.style.display = "grid";
+    creditScreen.style.display = 'grid';
 }
 
 
@@ -221,7 +223,7 @@ function displayCreditScreen() {
 
 // Event listener that executes when the page has loaded to start the game.
 // This event listener displays the splash screen for the set timeout period.
-window.addEventListener("load" , event => {
+window.addEventListener('load' , event => {
 //    window.setTimeout(hideSplash, 30000);
     window.setTimeout(hideSplash, 500);
 });
@@ -229,8 +231,8 @@ window.addEventListener("load" , event => {
 // Event listener for the accept mission yes button. 
 // If clicked, the gameboard will be displayed with the six countries available
 // to select.
-let missionYesButton = document.getElementById("mission-yes-button");
-missionYesButton.addEventListener("click", event => {
+let missionYesButton = document.getElementById('mission-yes-button');
+missionYesButton.addEventListener('click', event => {
     hideMissionScreen();
     highlightGameboard();
 });
@@ -238,8 +240,8 @@ missionYesButton.addEventListener("click", event => {
 // Event listener for the accept mission no button. 
 // If clicked, the confirmation screen will be displayed to verify the player
 // doesn't want to accept the mission (ending the game).
-let missionNoButton = document.getElementById("mission-no-button");
-missionNoButton.addEventListener("click", event => {
+let missionNoButton = document.getElementById('mission-no-button');
+missionNoButton.addEventListener('click', event => {
     hideMissionScreen();
     displayConfirmationScreen();
 });
@@ -247,61 +249,61 @@ missionNoButton.addEventListener("click", event => {
 // Event listener for country selection button 1. 
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country1Button = document.getElementById("country-button-1");
-country1Button.addEventListener("click", event => {
+let country1Button = document.getElementById('country-button-1');
+country1Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country1");
+    executeMission('country1');
 });
 
 // Event listener for country selection button 2. 
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country2Button = document.getElementById("country-button-2");
-country2Button.addEventListener("click", event => {
+let country2Button = document.getElementById('country-button-2');
+country2Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country2");
+    executeMission('country2');
 });
 
 // Event listener for country selection button 3.
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country3Button = document.getElementById("country-button-3");
-country3Button.addEventListener("click", event => {
+let country3Button = document.getElementById('country-button-3');
+country3Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country3");
+    executeMission('country3');
 });
 
 // Event listener for country selection button 4.
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country4Button = document.getElementById("country-button-4");
-country4Button.addEventListener("click", event => {
+let country4Button = document.getElementById('country-button-4');
+country4Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country4");
+    executeMission('country4');
 });
 
 // Event listener for country selection button 5.
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country5Button = document.getElementById("country-button-5");
-country5Button.addEventListener("click", event => {
+let country5Button = document.getElementById('country-button-5');
+country5Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country5");
+    executeMission('country5');
 });
 
 // Event listener for country selection button 6.
 // If clicked, the mission execution screen will be displayed for the selected
 // country.
-let country6Button = document.getElementById("country-button-6");
-country6Button.addEventListener("click", event => {
+let country6Button = document.getElementById('country-button-6');
+country6Button.addEventListener('click', event => {
     hideCountryScreen();
-    executeMission("country6");
+    executeMission('country6');
 });
 
 // Event listener for the confirmation screen 'yes' button.
 // If clicked, the game statistics screen will be displayed.
-let confirmYesButton = document.getElementById("confirm-yes-button");
-confirmYesButton.addEventListener("click", event => {
+let confirmYesButton = document.getElementById('confirm-yes-button');
+confirmYesButton.addEventListener('click', event => {
     hideConfirmationScreen();
     displayStatisticsScreen();
 });
@@ -310,16 +312,16 @@ confirmYesButton.addEventListener("click", event => {
 // If clicked, the mission screen will be displayed without generating a new
 // mission. (The mission the player rejected will be displayed again, in order
 // to prevent gaming the logic by rejecting a mission and generating a new one.)
-let confirmNoButton = document.getElementById("confirm-no-button");
-confirmNoButton.addEventListener("click", event => {
+let confirmNoButton = document.getElementById('confirm-no-button');
+confirmNoButton.addEventListener('click', event => {
     hideConfirmationScreen();
     displayMissionScreen();
 });
 
 // Event listener for the play again screen 'yes' button.
 // If clicked, a new game will be started, bypassing the spash screen.
-let playAgainYesButton = document.getElementById("play-again-yes-button");
-playAgainYesButton.addEventListener("click", event => {
+let playAgainYesButton = document.getElementById('play-again-yes-button');
+playAgainYesButton.addEventListener('click', event => {
     hidePlayAgainScreen();
     clearGameStats();
     generateMission();
@@ -328,8 +330,8 @@ playAgainYesButton.addEventListener("click", event => {
 
 // Event listener for the play again screen 'no' button.
 // If clicked. the credit screen will be displayed.
-let playAgainNoButton = document.getElementById("play-again-no-button");
-playAgainNoButton.addEventListener("click", event => {
+let playAgainNoButton = document.getElementById('play-again-no-button');
+playAgainNoButton.addEventListener('click', event => {
     hidePlayAgainScreen();
     displayCreditScreen();
 });
